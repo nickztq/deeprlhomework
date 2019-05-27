066346dfc82ac420478bdb073cd5a23251527f74
Tianqi <nickztq@gmail.com>
2019-05-27 16:06:10 -0400

    hw2_problem2

--- hw2/train_pg_f18.py
+++ hw2/train_pg_f18.py
@@ -5,6 +5,7 @@ Adapted for CS294-112 Fall 2018 by Michael Chang and Soroush Nasiriany
 """
 import numpy as np
 import tensorflow as tf
+import tensorflow_probability as tfp
 import gym
 import logz
 import os
@@ -38,7 +39,10 @@ def build_mlp(input_placeholder, output_size, scope, n_layers, size, activation=
         Hint: use tf.layers.dense    
     """
     # YOUR CODE HERE
-    raise NotImplementedError
+    fwd = input_placeholder
+    for _ in range(n_layers):
+        fwd = tf.layers.dense(fwd, size, activation=activation)
+    output_placeholder = tf.layers.dense(fwd, output_size, activation=output_activation)
     return output_placeholder
 
 def pathlength(path):
@@ -95,14 +99,13 @@ class Agent(object):
                 sy_ac_na: placeholder for actions
                 sy_adv_n: placeholder for advantages
         """
-        raise NotImplementedError
         sy_ob_no = tf.placeholder(shape=[None, self.ob_dim], name="ob", dtype=tf.float32)
         if self.discrete:
             sy_ac_na = tf.placeholder(shape=[None], name="ac", dtype=tf.int32) 
         else:
             sy_ac_na = tf.placeholder(shape=[None, self.ac_dim], name="ac", dtype=tf.float32) 
         # YOUR CODE HERE
-        sy_adv_n = None
+        sy_adv_n = tf.placeholder(shape=[None], name='adv', dtype=tf.float32)
         return sy_ob_no, sy_ac_na, sy_adv_n
 
 
@@ -134,15 +137,27 @@ class Agent(object):
                 Pass in self.n_layers for the 'n_layers' argument, and
                 pass in self.size for the 'size' argument.
         """
-        raise NotImplementedError
         if self.discrete:
             # YOUR_CODE_HERE
-            sy_logits_na = None
-            return sy_logits_na
+            sy_logits_na = build_mlp(input_placeholder=sy_ob_no, 
+                                     output_size=self.ac_dim, 
+                                     scope=None, 
+                                     n_layers=self.n_layers, 
+                                     size=self.size, 
+                                     activation=tf.nn.relu, 
+                                     output_activation=tf.nn.softmax)
+            return tf.log(sy_logits_na)
         else:
             # YOUR_CODE_HERE
-            sy_mean = None
-            sy_logstd = None
+            sy_mean = build_mlp(input_placeholder=sy_ob_no, 
+                                output_size=self.ac_dim, 
+                                scope=None, 
+                                n_layers=self.n_layers, 
+                                size=self.size, 
+                                activation=tf.nn.relu, 
+                                output_activation=None)
+            sy_logstd = tf.get_varible(name='log_std', shape=[self.ac_dim], 
+                initializer=tf.constant_initializer(1.0))
             return (sy_mean, sy_logstd)
 
     #========================================================================================#
@@ -172,15 +187,15 @@ class Agent(object):
         
                  This reduces the problem to just sampling z. (Hint: use tf.random_normal!)
         """
-        raise NotImplementedError
         if self.discrete:
             sy_logits_na = policy_parameters
             # YOUR_CODE_HERE
-            sy_sampled_ac = None
+            sy_sampled_ac = tf.random.multinomial(sy_logits_na, 1)
         else:
             sy_mean, sy_logstd = policy_parameters
             # YOUR_CODE_HERE
-            sy_sampled_ac = None
+            sy_sampled_ac = tf.random.normal(tf.shape(sy_mean))
+            sy_sampled_ac = sy_mean + sy_logstd * sy_sampled_ac
         return sy_sampled_ac
 
     #========================================================================================#
@@ -209,15 +224,21 @@ class Agent(object):
                 For the discrete case, use the log probability under a categorical distribution.
                 For the continuous case, use the log probability under a multivariate gaussian.
         """
-        raise NotImplementedError
         if self.discrete:
             sy_logits_na = policy_parameters
             # YOUR_CODE_HERE
-            sy_logprob_n = None
+            # sy_logits_na: (batch_size, self.ac_dim)
+            # sy_ac_na: [[0,3,4,1]] of size batch size
+            dist = tfp.distributions.Categorical(sy_logits_na)
+            sy_logprob_n = dist.log_prob(tf.reshape(sy_ac_na, [-1]))
         else:
             sy_mean, sy_logstd = policy_parameters
             # YOUR_CODE_HERE
-            sy_logprob_n = None
+            # sy_mean: (batch_size, self.ac_dim)
+            # sy_logstd: [[x,x,x]] of size action_dim
+            # sy_ac_na: (batch_size, self, ac_dim)
+            dist = tfp.distributions.Normal(sy_mean, sy_logstd)
+            sy_logprob_n = dist.log_prob(sy_ac_na)
         return sy_logprob_n
 
     def build_computation_graph(self):
